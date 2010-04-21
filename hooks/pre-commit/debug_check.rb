cc94ad1b96a2d3ad74989c22032e9de586500e65
Ryan Burrows <rhburrows@gmail.com>
2010-04-20 21:29:37 -0700

    Update sample scripts to new api

--- hooks/pre-commit/debug_check.rb
+++ hooks/pre-commit/debug_check.rb
@@ -1,20 +1,15 @@
-require 'rubygems'
-require 'ruby-debug'
-
 class DebugCheck
-  def call(repo)
-    return true
-#    diff = repo.commits.last.to_patch
-#    diff.split(/^--- .+$/).inject(true) do |result, d|
-#      if d.match(/^\+.*debugger/)
-#        filename = d.match(/^\+\+\+ b\/(.+)$/)[1]
-#        puts "Found a debugger in file: #{filename}"
-#        puts d
-#        puts "\n"
-#        false
-#      else
-#        result
-#      end
-#    end
+  def call(changes)
+    changes.raw.split(/^--- .+$/).inject(true) do |result, d|
+      if d.match(/^\+.*debugger/)
+        filename = d.match(/^\+\+\+ b\/(.+)$/)[1]
+        puts "Found a debugger in file: #{filename}"
+        puts d
+        puts "\n"
+        false
+      else
+        result
+      end
+    end
   end
 end
--- hooks/pre-commit/whitespace.rb
+++ hooks/pre-commit/whitespace.rb
@@ -1,8 +1,8 @@
 require 'fileutils'
+
 class Whitespace
-  def call(x)
-    file_names = x[1]
-    file_names.each  do |file|
+  def call(changes)
+    changes.files.each  do |file|
       whole_text = ""
       File.open(file,"r").readlines.each do |line|
         whole_text += line.rstrip + "\n"
